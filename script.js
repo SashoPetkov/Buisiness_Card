f43f10a68cd18eff9f819a05536debd748a1452f
SashoPetkov <petkovsasho@gmail.com>
2016-12-27 22:11:22 +0200

    commit

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
-$('section:nth-of-type(2)').click(function(){
-    $('.drop1').toggle(1000);  
-} );
-$('section:nth-of-type(3)').click(function(){
-    $('.drop2').toggle(1000);  
-} );
-$('section:nth-of-type(4)').click(function(){
-    $('.drop3').toggle(1000);  
-} );
+(function (){
+    $('h2').on('mouseover', function(){
+        $(this).css('cursor', 'pointer');
+    });
+    function clickSection (numSection){
+        var clicks = true;
+        $(numSection).on('click', function(){
+            var myChild = $(numSection).children().last(); 
+            if (clicks) {
+                myChild.show();
+                clicks = false;
+            } else {
+                myChild.hide();
+                clicks = true;
+            }
+        });
+    }
+    clickSection('section:nth-of-type(2)');
+    clickSection('section:nth-of-type(3)');
+    clickSection('section:nth-of-type(4)');
 
-
+    var fullWorkingSite = $('<a href="https://github.com/SashoPetkov/myProject"></a>').appendTo('section:nth-of-type(3) article');
+    fullWorkingSite.css({
+                            'display': 'block',
+                            'text-align': 'center',
+                            'margin': '20px 0',
+                            'text-decoration': 'none',
+                            'color': 'red'
+                        });
+    fullWorkingSite.text('click here to see full working site on gitHub (HTML + CSS + JavaScript(jQuery)');
+})();
